1c315e1c5f0af94abe099154a46fbd8f99496274
will jones <will jones>
2016-12-23 17:32:06 -0500

    tests

--- src/app/recipe-add/recipe-add.component.spec.ts
+++ src/app/recipe-add/recipe-add.component.spec.ts
@@ -16,12 +16,14 @@ import { RecipeService } from '../shared/recipe-service/recipe.service';
 import { ThemeService } from '../shared/theme-service/theme.service';
 
 import { RouterLinkStubDirective, ActivatedRouteStub, RouterStub } from '../shared/testing/routerstubs';
-import { RecipeServiceMock } from '../shared/recipe-service/recipe.service.mock';
 import { Firemocksvc } from '../shared/testing/firemock';
+import { click } from '../shared/testing/click';
 
-describe('RecipeEditComponent', () => {
+describe('RecipeAddComponent', () => {
     let component: RecipeAddComponent;
     let fixture: ComponentFixture<RecipeAddComponent>;
+
+    let saveSpy: jasmine.Spy;
 
     let actrt = new ActivatedRouteStub();
     actrt.testParams = {
@@ -41,7 +43,7 @@ describe('RecipeEditComponent', () => {
                 RouterLinkStubDirective,
             ],
             providers: [
-                { provide: RecipeService, useClass: RecipeServiceMock },
+                RecipeService,
                 ThemeService,
                 { provide: AngularFire, useClass: Firemocksvc },
                 { provide: ActivatedRoute, useValue: actrt },
@@ -54,10 +56,83 @@ describe('RecipeEditComponent', () => {
     beforeEach(() => {
         fixture = TestBed.createComponent(RecipeAddComponent);
         component = fixture.componentInstance;
+
+        let recsvc = fixture.debugElement.injector.get(RecipeService);
+        saveSpy = spyOn(recsvc, 'addRecipe').and.returnValue(Promise.resolve({ key: "thenewkeyeyey" }));
+
         fixture.detectChanges();
     });
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it("should have places to enter main recipe things", () => {
+        let des = fixture.debugElement.queryAll(By.css('md-input'));
+
+        expect(des[0].nativeElement.innerHTML).toContain("Recipe Title");
+        expect(des[1].nativeElement.innerHTML).toContain("Image");
+        expect(des[2].nativeElement.innerHTML).toContain("Yield");
+        expect(des[3].nativeElement.innerHTML).toContain("Time");
+
+    });
+
+    it("should have the ability to add ingredients", () => {
+        let de = fixture.debugElement.query(By.css('.add-ing'));
+
+        expect(component.recipe.ingredients.length).toBe(0);
+
+        click(de.nativeElement);
+
+        expect(component.recipe.ingredients.length).toBe(1);
+    });
+
+    it("should have the ability to add recipe steps", () => {
+        let de = fixture.debugElement.query(By.css('.add-step'));
+
+        expect(component.recipe.steps.length).toBe(0);
+
+        click(de.nativeElement);
+
+        expect(component.recipe.steps.length).toBe(1);
+    });
+
+    it("should have a cancel button that doesn't save", () => {
+        let de = fixture.debugElement.query(By.css('.cancel-btn'));
+        let route = de.injector.get(RouterLinkStubDirective);
+
+        expect(route.navigatedTo).toBeNull;
+
+        click(de.nativeElement);
+
+        expect(route.navigatedTo).toBe('/recipes');
+
+    });
+
+    it("should have a save button that saves and routes to the new recipe", () => {
+        // add a step
+        let de = fixture.debugElement.query(By.css('.add-step'));
+        click(de.nativeElement);
+        fixture.detectChanges();
+        expect(component.recipe.steps.length).toBe(1);
+
+        // add an ing
+        de = fixture.debugElement.query(By.css('.add-ing'));
+        click(de.nativeElement);
+        fixture.detectChanges();
+        expect(component.recipe.ingredients.length).toBe(1);
+
+        // click the save
+        de = fixture.debugElement.query(By.css('.save-btn'));
+        let route = de.injector.get(Router);
+
+        fixture.detectChanges();
+
+        expect(route.navigatedTo).toBeNull;
+
+        click(de.nativeElement);
+        fixture.whenStable().then(() => {
+            expect(route.navigatedTo).toEqual(['/recipe/thenewkeyeyey']);
+        });
     });
 });
--- src/app/shared/recipe-service/recipe.service.mock.ts
+++ src/app/shared/recipe-service/recipe.service.mock.ts
@@ -16,19 +16,12 @@ export class RecipeServiceMock {
         this._authed.next(true);
     }
 
+    newRec = {
+        key: "thenewkey"
+    };
+
     addRecipe(recipe) {
-        return new Observable((observer) => {
-            observer.next({
-                "image": "https://s3.amazonaws.com/pp-test-img/elk-c.png",
-                "ingredients": ["1 onion, chopped", "3 cloves garlic, minced", "1 tablespoon olive oil", "2 teaspoons chili powder", "1 teaspoon dried cumin", "1 (28 ounce) can crushed tomatoes", "28-32oz chicken broth", "1 can whole corn kernels", "1 can white hominy", "2 (7 ounce) can chopped green chile peppers", "1 (15 ounce) can black beans, rinsed and drained", "1/4 cup chopped fresh cilantro", "2 boneless chicken breast halves, cooked and cut into bite-sized pieces", "Sharp Cheddar Cheese"],
-                "steps": ["In a medium stock pot, heat oil over medium heat. Saute onion and garlic in oil until soft.", "Stir in chili powder, cumin, tomatoes, broth, and water.", "Bring to a boil, and simmer for 5 to 10 minutes.", "Stir in corn, hominy, chiles, beans, cilantro, and chicken. Simmer for 1 hour.", "Ladle soup into individual serving bowls, and top with cheese."],
-                "time": "1.5 hr",
-                "title": "Chicken Tortilla Soup",
-                "yield": "1 pot"
-            }
-            );
-            observer.complete();
-        });
+        return Promise.resolve(this.newRec);
     }
 
     getRecipes(): any {
