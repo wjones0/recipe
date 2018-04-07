4902ce6123db9ba3e8be3f9f895932b826844514
will jones <wjones0@users.noreply.github.com>
2018-04-07 15:12:41 -0400

    trying to add dialogflow! with TS..

---
+++ functions/src/index.ts
@@ -0,0 +1,19 @@
+import * as functions from 'firebase-functions';
+import { DialogflowApp } from 'actions-on-google';
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+export const helloWorld = functions.https.onRequest((request, response) => {
+
+    let dialogApp = new DialogflowApp({ request, response });
+
+    function ingredientList(app: DialogflowApp) {
+        app.tell(`I see you want to know about ${app.getArgument('recipe_name')}`);
+    }
+
+    let actionMap = new Map();
+    actionMap.set('IngredientList', ingredientList);
+
+    dialogApp.handleRequest(actionMap);
+});
